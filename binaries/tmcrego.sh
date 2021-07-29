4166465dbb32d7bf9ae3da21fc18f35dbd63629b
Ali Nahid <alinahid@gmail.com>
2021-07-29 18:52:56 +1000

    script error correction

--- binaries/tmcrego.sh
+++ binaries/tmcrego.sh
@@ -32,7 +32,7 @@ then
 
     printf "\n\n\n***********Adjusting Registration yaml with link and tmc svc name...*************\n"
     sed -ri 's/^(\s*)(namespace\s*:\s*tmcsvc\s*$)/\1namespace: '$TMCSVCNAME'/' /tmp/tmc-registration.yaml
-    sed -i 's,tmcregistrationlink,'$TMC_REGISTRATION_LINK',g' /tmp/tmc-registration.yaml
+    awk -v old="tmcregistrationlink" -v new="$TMC_REGISTRATION_LINK" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' /tmp/tmc-registration.yaml > /tmp/t.yaml && mv /tmp/t.yaml /tmp/tmc-registration.yaml
 
     printf "\n\n\n***********Applying registration yaml...*************\n"
     kubectl create -f /tmp/tmc-registration.yaml
@@ -45,7 +45,7 @@ then
 
     printf "\n\n\nWhen the status: line at the bottom of the output changes from INSTALLATION_IN_PROGRESS to INSTALLED, the installation is complete.\n"
     printf "\nIf the status comes out to be IN_PROGRESS here then run the below command to check progress again few mins later:\n"
-
+    printf "\n\nkubectl -n $TMCSVCNAME describe agentinstall tmc-agent-installer-config\n"
     printf "\n\nCOMPLETE. Thank you.\n"
 
     printf "\nCOMPLETE=YES" >> /root/.env
