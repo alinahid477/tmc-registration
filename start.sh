49e67b58eab2af78553dcc2ca954fe63450544b3
Ali Nahid <alinahid@gmail.com>
2021-11-17 08:18:58 +1100

    start script

---
+++ start.sh
@@ -0,0 +1,20 @@
+name=$1
+forcebuild=$2
+if [[ $name == "forcebuild" ]]
+then
+    name=''
+    forcebuild='forcebuild'
+fi
+
+if [[ -z $name ]]
+then
+    printf "\nAssuming default name is: tmcrego\n"
+    name='tmcrego'
+fi
+
+isexists=$(docker images | grep "\<$name\>")
+if [[ -z $isexists || $forcebuild == "forcebuild" ]]
+then
+    docker build . -t $name
+fi
+docker run -it --rm -v ${PWD}:/root/ --add-host kubernetes:127.0.0.1 --name $name $name
