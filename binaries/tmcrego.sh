3be37ae9f7d73e0f758d8c44d2020780ebf32423
Ali Nahid <alinahid@gmail.com>
2022-03-06 12:42:23 +1100

    tkgm registration

--- binaries/tmcrego.sh
+++ binaries/tmcrego.sh
@@ -46,6 +46,35 @@ then
             ssh -i /root/.ssh/id_rsa -4 -fNT -L 6443:$TKG_SUPERVISOR_ENDPOINT:6443 $BASTION_USERNAME@$BASTION_HOST
         fi
     fi
+    if [ -z "$COMPLETE" ]
+    then
+        printf "\n\n\n***********Getting TMC SVC Name...*************\n"
+        TMCSVCNAME="$(kubectl get ns -o=jsonpath='{range .items..metadata}{.name}{"\n"}{end}' | grep svc-tmc)"
+        echo "tmc svc name: $TMCSVCNAME"
+
+        printf "\n\n\n***********Adjusting Registration yaml with link and tmc svc name...*************\n"
+        sed -ri 's/^(\s*)(namespace\s*:\s*tmcsvc\s*$)/\1namespace: '$TMCSVCNAME'/' /tmp/tmc-registration.yaml
+        awk -v old="tmcregistrationlink" -v new="$TMC_REGISTRATION_LINK" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' /tmp/tmc-registration.yaml > /tmp/t.yaml && mv /tmp/t.yaml /tmp/tmc-registration.yaml
+
+        printf "\n\n\n***********Applying registration yaml...*************\n"
+        kubectl create -f /tmp/tmc-registration.yaml
+
+        printf "\n\n\n***********Giving it 3 mins...*************\n"
+        sleep 3m
+
+        printf "\n\n\n***********Checking status...*************\n"
+        kubectl -n $TMCSVCNAME describe agentinstall tmc-agent-installer-config
+
+        printf "\n\n\nWhen the status: line at the bottom of the output changes from INSTALLATION_IN_PROGRESS to INSTALLED, the installation is complete.\n"
+        printf "\nIf the status comes out to be IN_PROGRESS here then run the below command to check progress again few mins later:\n"
+        printf "\n\nkubectl -n $TMCSVCNAME describe agentinstall tmc-agent-installer-config\n"
+        printf "\n\nCOMPLETE. Thank you.\n"
+
+        printf "\nCOMPLETE=YES" >> /root/.env
+    else
+        printf "\n\n\nTMC Registration is already marked as complete. (If this is not desired please change COMPLETE=\"\" or remove COMPLETE in the .env for new registration)\n"
+        printf "\n\n\nGoing straight to shell access.\n"
+    fi
 else
     if [[ -n $TKG_MANAGEMENT_ENDPOINT ]]
     then
@@ -55,42 +84,15 @@ else
         then
             printf "\n\n\n***********Creating Tunnel through bastion $BASTION_USERNAME@$BASTION_HOST ...*************\n"
             ssh -i /root/.ssh/id_rsa -4 -fNT -L 6443:$TKG_MANAGEMENT_ENDPOINT:6443 $BASTION_USERNAME@$BASTION_HOST
-        fi  
+        fi
+
+        kubectl apply -f "$TMC_REGISTRATION_LINK"
+    else
+        printf "\n\n\nERROR: You must specify either TKG_SUPERVISOR_ENDPOINT or TKG_MANAGEMENT_ENDPOINT\n\n"
+        exit
     fi
+
 fi
 
-
-
-
-
-if [ -z "$COMPLETE" ]
-then
-    printf "\n\n\n***********Getting TMC SVC Name...*************\n"
-    TMCSVCNAME="$(kubectl get ns -o=jsonpath='{range .items..metadata}{.name}{"\n"}{end}' | grep svc-tmc)"
-    echo "tmc svc name: $TMCSVCNAME"
-
-    printf "\n\n\n***********Adjusting Registration yaml with link and tmc svc name...*************\n"
-    sed -ri 's/^(\s*)(namespace\s*:\s*tmcsvc\s*$)/\1namespace: '$TMCSVCNAME'/' /tmp/tmc-registration.yaml
-    awk -v old="tmcregistrationlink" -v new="$TMC_REGISTRATION_LINK" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' /tmp/tmc-registration.yaml > /tmp/t.yaml && mv /tmp/t.yaml /tmp/tmc-registration.yaml
-
-    printf "\n\n\n***********Applying registration yaml...*************\n"
-    kubectl create -f /tmp/tmc-registration.yaml
-
-    printf "\n\n\n***********Giving it 3 mins...*************\n"
-    sleep 3m
-
-    printf "\n\n\n***********Checking status...*************\n"
-    kubectl -n $TMCSVCNAME describe agentinstall tmc-agent-installer-config
-
-    printf "\n\n\nWhen the status: line at the bottom of the output changes from INSTALLATION_IN_PROGRESS to INSTALLED, the installation is complete.\n"
-    printf "\nIf the status comes out to be IN_PROGRESS here then run the below command to check progress again few mins later:\n"
-    printf "\n\nkubectl -n $TMCSVCNAME describe agentinstall tmc-agent-installer-config\n"
-    printf "\n\nCOMPLETE. Thank you.\n"
-
-    printf "\nCOMPLETE=YES" >> /root/.env
-else
-    printf "\n\n\nTMC Registration is already marked as complete. (If this is not desired please change COMPLETE=\"\" or remove COMPLETE in the .env for new registration)\n"
-    printf "\n\n\nGoing straight to shell access.\n"
-fi
 
 /bin/bash
